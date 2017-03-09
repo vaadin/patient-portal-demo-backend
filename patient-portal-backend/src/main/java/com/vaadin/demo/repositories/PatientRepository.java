fcd25ee8cf2140d3b87ed38faa9d177490947a6f
Devarsh Desai <devarsh@vaadin.com>
2017-03-09 11:55:43 -0800

    Fixed query

--- patient-portal-backend/src/main/java/com/vaadin/demo/repositories/PatientRepository.java
+++ patient-portal-backend/src/main/java/com/vaadin/demo/repositories/PatientRepository.java
@@ -7,12 +7,32 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    @Query("SELECT COUNT(p), '21-30' AS c FROM Patient p WHERE (p.birthDate >= '1987-01-01' AND p.birthDate <= '1997-01-01') UNION " +
-            "SELECT COUNT(p), '31-40' AS c FROM Patient p WHERE (p.birthDate >= '1977-01-01' AND p.birthDate <= '1987-01-01') UNION " +
-            "SELECT COUNT(p2), '41-50' AS c  FROM Patient p2 WHERE p2.birthDate >= '1967-01-01' AND p2.birthDate <= '1977-01-01' UNION " +
-            "SELECT COUNT(p3), '51-60' AS c  FROM Patient p3 WHERE p3.birthDate >= '1957-01-01' AND p3.birthDate <= '1967-01-01' UNION " +
-            "SELECT COUNT(p4), '61-70' AS c  FROM Patient p4 WHERE p4.birthDate >= '1947-01-01' AND p4.birthDate <= '1957-01-01' UNION " +
-            "SELECT COUNT(p5), '71-80' AS c  FROM Patient p5 WHERE p5.birthDate >= '1937-01-01' AND p5.birthDate <= '1947-01-01'")
+    @Query(value = "select tuples.\"range\", count(ageList.age) as \"number of occurences\"\n" +
+            "  from " +
+            "  ( " +
+            "     select 0 min, 9 max, '0-9' \"range\"\n" +
+            "     union all\n" +
+            "     select 10, 19, '10-19'\n" +
+            "     union all\n" +
+            "     select 20, 29, '20-29'\n" +
+            "     union all\n" +
+            "     select 30, 39, '30-39'\n" +
+            "     union all\n" +
+            "     select 40, 49, '40-49'\n" +
+            "     union all\n" +
+            "     select 50, 59, '50-59'\n" +
+            "     union all\n" +
+            "     select 60, 69, '60-69'\n" +
+            "     union all \n " +
+            "     select 70, 79, '70-79'\n" +
+            "     union all\n" +
+            "     select 80, 89, '80-89'\n" +
+            "     union all\n" +
+            "     select 90, 99, '90-99'\n" +
+            "  ) as tuples\n" +
+            "  left join (SELECT EXTRACT(years from age(CURRENT_DATE , CAST(p.birthDate AS DATE))) as age FROM Patient p) AS ageList\n" +
+            "    on ageList.age between tuples.min and tuples.max \n" +
+            " group by tuples.range ORDER BY tuples.range", nativeQuery = true)
     List<Object[]> queryGetStatsByAge();
 
     @Query("SELECT COUNT(p), p.gender FROM Patient p GROUP BY p.gender")
@@ -20,4 +40,5 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     @Query("SELECT COUNT(p), p.doctor FROM Patient p GROUP BY p.doctor ")
     List<Object[]> queryGetStatsByDoctor();
+
 }
