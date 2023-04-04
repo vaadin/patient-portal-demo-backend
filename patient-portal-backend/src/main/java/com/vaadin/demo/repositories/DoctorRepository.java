0c000254dfc6bc9db78c4ccfe0bd197e932533e3
taefi <taefi.soroosh@gmail.com>
2023-04-04 19:01:51 +0300

    chore: bump to 2.0.0-SNAPSHOT to align with jakarta and SB3 changes

--- patient-portal-backend/src/main/java/com/vaadin/demo/repositories/DoctorRepository.java
+++ patient-portal-backend/src/main/java/com/vaadin/demo/repositories/DoctorRepository.java
@@ -1,12 +1,7 @@
 package com.vaadin.demo.repositories;
 
 import com.vaadin.demo.entities.Doctor;
-import com.vaadin.demo.service.StringLongPair;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
